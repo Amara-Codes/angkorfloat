e1b7e4fa89cac4b5114f1c8339177634f39dc7bb
lotusdreamspa <lotusdreamspa.sr@gmail.com>
2026-05-22 12:41:59 +0700

    fix: removed generate static params

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -40,10 +40,6 @@ export const metadata: Metadata = {
 };
 
 
-export function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }));
-}
-
 export default async function RootLayout({
   children,
   params
